a341e786383fd2308f9e31c4267d5f93c6d2f089
Sheldin Lau <lausheldin@gmail.com>
2026-04-20 17:03:47 -0700

    feat: add renderer entry point and styles

---
+++ src/renderer/src/main.jsx
@@ -0,0 +1,14 @@
+import '@fontsource/jetbrains-mono/400.css'
+import '@fontsource/jetbrains-mono/500.css'
+import '@fontsource/jetbrains-mono/600.css'
+import '@fontsource/jetbrains-mono/700.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+import './styles.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
