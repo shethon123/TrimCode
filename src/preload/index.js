65325a0264d849dae980f47d912aa65889253dba
Sheldin Lau <lausheldin@gmail.com>
2026-04-22 15:45:08 -0700

    feat: expose fileExists via preload

--- src/preload/index.js
+++ src/preload/index.js
@@ -2,4 +2,5 @@ import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('electronAPI', {
   saveFile: (filename, content) => ipcRenderer.invoke('save-file', { filename, content }),
+  fileExists: (filename) => ipcRenderer.invoke('file-exists', filename),
 })
